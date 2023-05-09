11b09ced4969bf967cba2b1cf9be65f7f28a7937
Eric <ericleee777@gmail.com>
2023-05-09 10:27:33 -0400

    update menu and sidemenu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
+"use client"
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 const Navbar = () => {
   const path = usePathname();
-console.log(path)
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleClick = (e: any) => {
+      const menu = document.getElementById("menu");
+      if (!menu?.contains(e.target))
+        setShow(false)
+    }
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  })
+
   return (
     <div className="flex w-full h-20 bg-[#0E0F2DE5] px-2 lg:px-20 items-center justify-between">
       <img src="/images/logo.png" className="h-12" alt="logo" />
       <div className="hidden lg:flex gap-8 items-center">
         {Menus.map((menu, index) => (
-          <Link href={menu.route} key={index} 
-            className={` text-base opacity-${path?.includes(menu.route)? 100 : 50}`}
+          <Link href={menu.route} key={index}
+            className={`text-base relative group`}
+            style={{ opacity: path?.includes(menu.route) ? 1 : 0.5 }}
           >
             {menu.label}
+            {menu.submenu && <SubMenu menus={menu.submenu} />}
           </Link>
         ))}
         <button className="w-40 h-10 p-2 rounded-lg "
@@ -24,16 +38,62 @@ console.log(path)
           Build on Codex
         </button>
       </div>
+      <div className="block lg:hidden" id="menu">
+        <img
+          src="/images/mobile-menu-icon.svg" className="w-8" alt="menu"
+          onClick={() => setShow(true)}
+        />
+        <SideMenu show={show} />
+      </div>
     </div>
   )
 };
-
+const SubMenu = ({ menus }: { menus: any[] }) => {
+  const render = window != undefined
+  return (
+    <>
+      {render &&
+        <div className="hidden group-hover:flex flex-col w-50 p-1 bg-[#00084D80] rounded-sm absolute">
+          {menus.map((menu, index) => (
+            <Link href={menu.route} className="h-10 p-1 whitespace-nowrap border-b border-b-[#08ABF1] hover:bg-[#2B35C3]" key={index}>
+              <div>{menu.label}</div>
+            </Link>
+          ))}
+        </div>
+      }
+    </>
+  )
+}
+const SideMenu = ({ show }: { show: boolean }) => {
+  return (
+    <div
+      className={`fixed right-0 top-0 w-40 h-screen bg-white transition-all z-50`}
+      style={{ maxWidth: show ? "160px" : "0px" }}
+    >
+      Sidebar
+    </div>
+  )
+}
 export default Navbar;
 
 export const Menus = [
   {
     label: "Community",
-    route: "/community"
+    route: "/community",
+    submenu: [
+      {
+        label: "Codex Community",
+        route: "/codex-community"
+      },
+      {
+        label: "Discussion Forum",
+        route: "/codex-community"
+      },
+      {
+        label: "Ecosystem Projects",
+        route: "/codex-community"
+      },
+    ]
   },
   {
     label: "Developers",
